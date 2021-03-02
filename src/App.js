6ec080926adefe7d93b4f2bef7388c64f739efe3
Mian-Sajid <mianhouse255@gmail.com>
2021-03-02 12:33:47 +0500

    MT

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
           <h1>This is My Raect App</h1>
+          <p>Thus is my paragraohdhsi</p>
+          <p>yu</p>
+          <h1>Thus suiuid ui</h1>
     </div>
   );
 }
